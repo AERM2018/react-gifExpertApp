130494d219fa8157378442bdd0b639ace0a75c36
Angel Retana <retana.martinez.angel.edaurdo@gmail.com>
2020-08-25 00:04:02 -0500

    Crear carpeta docs

---
+++ docs/precache-manifest.dd84605ffbb0aa44fdd1ad50048b87bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77443ccf372b72f9fa57780ca5749868",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93ac8ab1804464eb3b04",
+    "url": "/static/css/main.021d32bf.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93ac8ab1804464eb3b04",
+    "url": "/static/js/main.6fa5be95.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
