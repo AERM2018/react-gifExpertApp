378dc457a560048acbdbd916735fbbfd07caf180
Angel Retana <40252881+AERM2018@users.noreply.github.com>
2020-09-12 17:29:30 -0500

    Update the limit of gifs

--- src/helpers/getGif.js
+++ src/helpers/getGif.js
@@ -1,5 +1,5 @@
 export const getGifs = async(categoria) =>{
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=Esj8Irv3dmowXGtfawNO4YNo2Gvivmfz&q=${encodeURI(categoria)}&limit=10`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=Esj8Irv3dmowXGtfawNO4YNo2Gvivmfz&q=${encodeURI(categoria)}&limit=20`;
     const resp = await fetch(url);
     const {data} = await resp.json();
 
